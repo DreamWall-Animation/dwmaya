5fa0f3f101d9f2f584445e4b87c36413ae7ec75c
Olivier Evers <evers@dreamwall.be>
2021-06-22 15:09:48 +0200

    hierarchy: fix get_shape_and_transform

--- dwmaya/hierarchy.py
+++ dwmaya/hierarchy.py
@@ -12,7 +12,7 @@ def get_shape_and_transform(shape_or_transform):
         transform = mc.listRelatives(shape, parent=True, path=True)[0]
     else:
         transform = shape_or_transform
-        transform = mc.listRelatives(transform, children=True, path=True)[0]
+        shape = mc.listRelatives(transform, children=True, path=True)[0]
     return shape, transform
 
 
