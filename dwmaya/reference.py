abcc2aaa9da8ec5d15b8b05edba84ebc0c238b2b
Olivier Evers <evers@dreamwall.be>
2021-11-26 14:51:31 +0100

    reference: add load/unload examples

--- dwmaya/reference.py
+++ dwmaya/reference.py
@@ -13,51 +13,61 @@ from dwmaya.hierarchy import get_closest_to_root
 from dwmaya.qt import chose_from_list_prompt
 
 
-def add_reference(path, namespace, parent=None):
-    mc.file(path, reference=True, prompt=False, namespace=namespace)
+def add_reference(path, namespace, group=None):
+    mc.file(
+        path, reference=True, prompt=False, namespace=namespace,
+        groupReference=bool(group), groupName=group)
 
 
-def remove_reference(refnode):
-    path = mc.referenceQuery(refnode, filename=True)
+def unload_reference(reference_node, force=True):
+    mc.file(unloadReference=reference_node, force=force)
+
+
+def load_reference(reference_node):
+    mc.file(loadReference=reference_node)
+
+
+def remove_reference(reference_node):
+    path = mc.referenceQuery(reference_node, filename=True)
     mc.file(path, removeReference=True, force=True)
 
 
-def import_reference(refnode):
-    path = mc.referenceQuery(refnode, filename=True)
+def import_reference(reference_node):
+    path = mc.referenceQuery(reference_node, filename=True)
     mc.file(path, importReference=True, force=True)
 
 
-def lock_reference(ref_node, lock=True):
-    mc.file(unloadReference=ref_node, force=True)
-    mc.setAttr(ref_node + '.locked', lock)
-    mc.file(loadReference=ref_node)
+def lock_reference(reference_node, lock=True):
+    unload_reference(reference_node)
+    mc.setAttr(reference_node + '.locked', lock)
+    load_reference(reference_node)
 
 
 @contextmanager
-def unlocked_reference_context(ref_node):
-    is_locked = mc.getAttr(ref_node + '.locked')
+def unlocked_reference_context(reference_node):
+    is_locked = mc.getAttr(reference_node + '.locked')
     if is_locked:
-        lock_reference(ref_node, lock=False)
+        lock_reference(reference_node, lock=False)
     yield None
     if is_locked:
-        lock_reference(ref_node)
+        lock_reference(reference_node)
 
 
-def get_reference_nodes(refnode):
+def get_reference_nodes(reference_node):
     try:
-        return mc.referenceQuery(refnode, nodes=True, dagPath=True)
+        return mc.referenceQuery(reference_node, nodes=True, dagPath=True)
     except RuntimeError:
         return []
 
 
-def get_reference_root_nodes(refnode):
-    return get_closest_to_root(get_reference_nodes(refnode))
+def get_reference_root_nodes(reference_node):
+    return get_closest_to_root(get_reference_nodes(reference_node))
 
 
 def get_references():
     refs = []
     for ref in mc.ls(type="reference"):
-        if "sharedReferenceNode" in ref or "_UNKNOWN_REF_NODE_" in ref:
+        if "sharedReferenceNode" in ref or "_UNKNOWN_reference_node_" in ref:
             continue
         try:
             mc.referenceQuery(ref, filename=True)
@@ -99,10 +109,11 @@ def change_reference_path_prompt():
     This will only look for files in the same directory as current file but
     is easily modified.
     """
-    ref_node = chose_reference_from_scene_prompt()
-    if not ref_node:
+    reference_node = chose_reference_from_scene_prompt()
+    if not reference_node:
         return
-    ref_path = mc.referenceQuery(ref_node, filename=True, unresolvedName=True)
+    ref_path = mc.referenceQuery(
+        reference_node, filename=True, unresolvedName=True)
     ref_path = remove_reference_number(ref_path)
     resolved_path = os.path.expandvars(ref_path)
     directory = os.path.dirname(resolved_path)
@@ -120,4 +131,4 @@ def change_reference_path_prompt():
     if new_unresolved_path == ref_path:
         mc.warning('Skipping, no reference path change (%s).' % ref_path)
         return
-    mc.file(new_unresolved_path, loadReference=ref_node, prompt=False)
+    mc.file(new_unresolved_path, loadReference=reference_node, prompt=False)
