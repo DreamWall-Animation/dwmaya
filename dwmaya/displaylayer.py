81f0b14b42e7fbcf626a3cb925a09e707cc413fa
Lionel Brouyère <brouyere@dreamwall.be>
2022-05-06 10:12:50 +0200

    Fix clear_display_layers

--- dwmaya/displaylayer.py
+++ dwmaya/displaylayer.py
@@ -4,7 +4,9 @@ import maya.cmds as mc
 def clear_display_layers():
     layers = mc.ls(type='displayLayer')
     layers.remove('defaultLayer')
-    layers = [l for l in layers if not mc.referenceQuery(isNodeReferenced=l)]
+    layers = [
+        layer for layer in layers
+        if not mc.referenceQuery(layer, isNodeReferenced=True)]
     if not layers:
         return
     for layer in layers:
