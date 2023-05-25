bbf5b4affcb7fd30ad0d8ddf2efe5b0a575127ce
ndeebook <evers@dreamwall.be>
2023-05-25 21:52:57 +0200

    launch_mayapy_script: use subprocess.run + remove copy_current_environment

--- dwmaya/mayapy.py
+++ dwmaya/mayapy.py
@@ -1,25 +1,11 @@
 import os
-import sys
 import tempfile
 import subprocess
 
 
-def copy_current_environment():
-    pythonpaths = os.environ["PYTHONPATH"].split(os.pathsep)
-    pythonpaths = [p.replace("\\", "/") for p in pythonpaths]
-    for path in sys.path:
-        path = path.replace("\\", "/")
-        if path in pythonpaths:
-            continue
-        pythonpaths.append(path)
-    environment = os.environ.copy()
-    environment["PYTHONPATH"] = str(os.pathsep.join(pythonpaths))
-    return environment
-
-
 def launch_mayapy_script(
         mayapypath=None, script=None, scriptpath=None, environment=None,
-        arguments=None):
+        arguments=None, capture_output=True):
     arguments = arguments or []
     if script:
         scriptpath = f'{tempfile.NamedTemporaryFile().name}.py'
@@ -28,10 +14,7 @@ def launch_mayapy_script(
             print(scriptpath)
     elif scriptpath is None:
         raise ValueError('Please specify at least a script or a script path')
-    environment = environment or copy_current_environment()
-    return subprocess.Popen(
+    return subprocess.run(
         [mayapypath or 'mayapy', scriptpath] + arguments,
-        env=environment,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        bufsize=-1)
+        env=environment or os.environ.copy(),
+        capture_output=capture_output)
