55ccb6cc9dd0d763ca62d3b83e4055d049a4eadd
ndeebook <evers@dreamwall.be>
2024-02-09 12:03:34 +0100

    reset_windows_positions skip arg

--- dwmaya/ui/resetwindows.py
+++ dwmaya/ui/resetwindows.py
@@ -1,6 +1,9 @@
 import maya.cmds as mc
 
 
-def reset_windows_positions():
+def reset_windows_positions(skip=None):
+    skip = skip or ['MayaWindow']
     for window in mc.lsUI(windows=True):
+        if window in skip:
+            continue
         mc.window(window, edit=True, topLeftCorner=[0, 0])
